b45dd918facfb2a3cd9da8dee02879af701f82bc
Avinandan1996 <85864769+Avinandan1996@users.noreply.github.com>
2021-06-14 12:58:19 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let screen=document.getElementById('display');
+button=document.querySelectorAll('button');
+let screenvalue='';
+for(item of button){
+    item.addEventListener('click', (e)=>{
+     buttonText=e.target.innerText;
+if(buttonText=='X'){
+    buttonText='*';
+    screenvalue +=buttonText;
+    screen.value=screenvalue;
+}
+else if(buttonText=='C'){
+    screenvalue='';
+    screen.value=screenvalue;
+}
+else if(buttonText=='='){
+    screen.value=eval(screenvalue)
+}
+else{
+    screenvalue +=buttonText;
+    screen.value=screenvalue;
+}
+  
+ })
+}
